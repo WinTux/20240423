74bda9152742a7614816e4885357fbc0c387faf5
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2024-05-07 21:25:35 -0400

    Agregado de un segundo proyecto

---
+++ 20240507/Controllers/HistorialController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20240507.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistorialController : ControllerBase
+    {
+        public HistorialController() { }
+        [HttpPost]
+        public ActionResult Post() {
+            Console.WriteLine("Llega una peticion al servicio 20240507 (Campus)");
+            return Ok("Respuesta exitosa desde el controlador Historial de Campus");
+        }
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "valor 1", "valor 2" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "valor unico";
+        }
+    }
+}
